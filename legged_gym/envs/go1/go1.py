631693b4f8c9bd2652d976fce4326cafd480ec8e
Ivan Shargin <shargin.ia@phystech.edu>
2025-03-14 14:59:17 +0300

    go1 stands up on rear legs without base_lin_vel in obs

---
+++ legged_gym/envs/go1/go1.py
@@ -0,0 +1,58 @@
+import torch
+from legged_gym.utils.isaacgym_utils import get_euler_xyz
+from legged_gym.envs import LeggedRobot
+from isaacgym import gymtorch
+
+class Go1(LeggedRobot):
+    def compute_observations(self):
+        """ Computes observations
+        """
+        episode_time_buf = self.episode_length_buf * self.dt
+        self.obs_buf = torch.cat((  self.base_ang_vel  * self.obs_scales.ang_vel,
+                                    self.projected_gravity,
+                                    torch.clip(episode_time_buf, 0., 3.).unsqueeze(1),
+                                    (self.dof_pos - self.default_dof_pos) * self.obs_scales.dof_pos,
+                                    self.dof_vel * self.obs_scales.dof_vel,
+                                    self.actions
+                                    ),dim=-1)
+        # add noise if needed
+        if self.add_noise:
+            self.obs_buf += (2 * torch.rand_like(self.obs_buf) - 1) * self.noise_scale_vec
+        
+    def _get_noise_scale_vec(self, cfg):
+        """ Sets a vector used to scale the noise added to the observations.
+            [NOTE]: Must be adapted when changing the observations structure
+
+        Args:
+            cfg (Dict): Environment config file
+
+        Returns:
+            [torch.Tensor]: Vector of scales used to multiply a uniform distribution in [-1, 1]
+        """
+        noise_vec = torch.zeros_like(self.obs_buf[0])
+        self.add_noise = self.cfg.noise.add_noise
+        noise_scales = self.cfg.noise.noise_scales
+        noise_level = self.cfg.noise.noise_level
+        noise_vec[:3] = noise_scales.ang_vel * noise_level * self.obs_scales.ang_vel
+        noise_vec[3:6] = noise_scales.gravity * noise_level
+        noise_vec[6] = 0. # time
+        noise_vec[7:19] = noise_scales.dof_pos * noise_level * self.obs_scales.dof_pos
+        noise_vec[19:31] = noise_scales.dof_vel * noise_level * self.obs_scales.dof_vel
+        noise_vec[31:43] = 0. # previous actions
+        return noise_vec
+
+    def _reward_tracking_pitch(self):
+        # Tracking
+        base_quat = self.root_states[:, 3:7]
+        euler = get_euler_xyz(base_quat)
+        episode_time_buf = self.episode_length_buf * self.dt
+        pitch_command = torch.clip(episode_time_buf * self.cfg.commands.pitch / 3., self.cfg.commands.pitch, 0.)
+        error = torch.square(pitch_command - euler[:, 1]) + torch.square(self.cfg.commands.roll - euler[:, 0])
+        return torch.exp(-error/self.cfg.rewards.tracking_sigma)
+    
+    def _reward_hip_pos(self):
+        hip_names = ["FR_hip_joint", "FL_hip_joint"]
+        self.hip_indices = torch.zeros(len(hip_names), dtype=torch.long, device=self.device, requires_grad=False)
+        for i, name in enumerate(hip_names):
+            self.hip_indices[i] = self.dof_names.index(name)
+        return torch.sum(torch.square(self.dof_pos[:, self.hip_indices] - self.default_dof_pos[:, self.hip_indices]), dim=1)
